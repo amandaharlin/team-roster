e81c0ee64ee1d85cc64a25874511b16f6e59c097
Amanda Harlin <amanda.n.harlin@gmail.com>
2018-04-20 12:19:31 -0500

    edited the roster sum and pulled it out

--- src/App.js
+++ src/App.js
@@ -21,14 +21,15 @@ class Pez extends Component {
     const isActive = true;
     return (
       <Button
-        icon={" "}
+        key={this.props.color}
+        icon
         color={this.props.color}
-        size="tiny"
+        size="big"
         basic={!isActive}
         checked={isActive}
         onClick={(event, data) => {
           const { checked } = data;
-          console.log("d.checked", checked);
+          console.log("d.checked", checked, "color", this.props.color);
         }}
       />
     );
@@ -46,32 +47,94 @@ class App extends Component {
 
   renderRosterTable = () => {
     return (
-      <Table selectable>
-        <Table.Header>
-          <Table.Row textAlign="center">
-            <Table.HeaderCell>R</Table.HeaderCell>
-            <Table.HeaderCell>G</Table.HeaderCell>
-            <Table.HeaderCell>B</Table.HeaderCell>
-            <Table.HeaderCell>Name</Table.HeaderCell>
-            <Table.HeaderCell>Department</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          <Table.Row textAlign="center">
-            <Table.Cell collapsing>
-              <Pez color="red" />
-            </Table.Cell>
-            <Table.Cell collapsing>
-              <Pez color="green" />
-            </Table.Cell>
-            <Table.Cell collapsing>
-              <Pez color="blue" />
-            </Table.Cell>
-            <Table.Cell>Yolanda </Table.Cell>
-            <Table.Cell>Sales</Table.Cell>
-          </Table.Row>
-        </Table.Body>
-      </Table>
+      <div>
+        <Header>Roster List</Header>
+        <Table selectable>
+          <Table.Header>
+            <Table.Row textAlign="center">
+              <Table.HeaderCell>R</Table.HeaderCell>
+              <Table.HeaderCell>G</Table.HeaderCell>
+              <Table.HeaderCell>B</Table.HeaderCell>
+              <Table.HeaderCell>Name</Table.HeaderCell>
+              <Table.HeaderCell>Department</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            <Table.Row textAlign="center">
+              <Table.Cell collapsing>
+                <Pez color="red" />
+              </Table.Cell>
+              <Table.Cell collapsing>
+                <Pez color="green" />
+              </Table.Cell>
+              <Table.Cell collapsing>
+                <Pez color="blue" />
+              </Table.Cell>
+              <Table.Cell>Yolanda</Table.Cell>
+              <Table.Cell>Sales</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+      </div>
+    );
+  };
+
+  renderRosterSum = () => {
+    return (
+      <div>
+        <Header>Team Roster</Header>
+        <Table basic="very" celled collapsing>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Teams</Table.HeaderCell>
+              <Table.HeaderCell>Points</Table.HeaderCell>
+
+              <Table.HeaderCell>Progress Bar</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>
+                <Header as="h4" image>
+                  <Icon name={"winner"} size="mini" />
+                  <Header.Content>
+                    Red Team<Header.Subheader>Human Resources</Header.Subheader>
+                  </Header.Content>
+                </Header>
+              </Table.Cell>
+              <Table.Cell>22</Table.Cell>
+              <Table.Cell>22</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>
+                <Header as="h4" image>
+                  <Icon name={"user circle outline"} size="mini" />
+                  <Header.Content>
+                    Green Team
+                    <Header.Subheader>Fabric Design</Header.Subheader>
+                  </Header.Content>
+                </Header>
+              </Table.Cell>
+              <Table.Cell>15</Table.Cell> <Table.Cell>22</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>
+                <Header as="h4" image>
+                  <Icon name={"user circle outline"} size="mini" />
+                  <Header.Content>
+                    Blue Team
+                    <Header.Subheader>Accounting</Header.Subheader>
+                  </Header.Content>
+                </Header>
+              </Table.Cell>
+              <Table.Cell>13</Table.Cell>
+              <Table.Cell>22</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+        <div>Make the RosterColorComponent here</div>
+      </div>
     );
   };
 
@@ -86,15 +149,11 @@ class App extends Component {
     });
     return (
       <Container className="App">
-        <Header>Team Roster</Header>
-        <List>
-          <List.Item>Red</List.Item>
-          <List.Item>Green</List.Item>
-          <List.Item>Blue</List.Item>
-        </List>
-        <div>Roster Icon Color Component</div>
         <Divider hidden />
-        <Header>Roster List</Header>
+        {this.renderRosterSum()}
+
+        <Divider />
+
         {this.renderRosterTable()}
       </Container>
     );
